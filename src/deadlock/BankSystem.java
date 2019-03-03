0312e8a6251184d0f290bd5f6ae96c4044ad717a
Sevatkaaa <sevatkaaa@gmail.com>
2019-03-03 15:59:00 +0200

    Add logs

--- src/deadlock/BankSystem.java
+++ src/deadlock/BankSystem.java
@@ -3,19 +3,31 @@ package deadlock;
 import exceptions.NotEnoughBabloException;
 
 public class BankSystem {
-    public static void main(String[] args)  {
+    public static void main(String[] args) throws InterruptedException {
         BankAccount a = new BankAccount(500);
         BankAccount b = new BankAccount(2000);
 
         new Thread(() -> transfer(a, b, 100)).start();
         new Thread(() -> transfer(b, a, 200)).start();
+
+        Thread.sleep(1000);
+
+        System.out.println(a.getBablo());
+        System.out.println(b.getBablo());
+
     }
 
     private static void transfer(BankAccount a, BankAccount b, int money) {
         if (money > a.getBablo()) {
             throw new NotEnoughBabloException(a);
         }
+        System.out.println(String.format("%d money on 1 account", a.getBablo()));
+        System.out.println(String.format("%d money on 2 account", b.getBablo()));
+
         a.minus(money);
         b.plus(money);
+
+        System.out.println(String.format("%d money on 1 account", a.getBablo()));
+        System.out.println(String.format("%d money on 2 account", b.getBablo()));
     }
 }
