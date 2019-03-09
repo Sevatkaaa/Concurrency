cbba92282e565fd58afd1ab3fc6677af0ec182df
Sevatkaaa <sevatkaaa@gmail.com>
2019-03-09 09:55:30 +0200

    add result view from threads

--- src/deadlock/BankSystem.java
+++ src/deadlock/BankSystem.java
@@ -1,8 +1,12 @@
 package deadlock;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class BankSystem {
     public static void main(String[] args) throws InterruptedException {
@@ -12,13 +16,22 @@ public class BankSystem {
         Random random = new Random();
 
         ExecutorService executor = Executors.newFixedThreadPool(3);
+        List<Future<Boolean>> results = new ArrayList<>();
 
         for (int i = 0; i < 5; i++) {
-            executor.submit(new TransferService(a, b, random.nextInt(100)));
-            executor.submit(new TransferService(b, a, random.nextInt(200)));
+            results.add(executor.submit(new TransferService(a, b, random.nextInt(100))));
+            results.add(executor.submit(new TransferService(b, a, random.nextInt(200))));
         }
 
         Thread.sleep(1000);
+
+        for (int i = 0; i < results.size(); i++) {
+            try {
+                System.out.println(results.get(i).get());
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
 
         executor.shutdown();
 
